53eb14bea3c76c846cd104b91a7d1e382dca8a98
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-04-27 23:35:43 +0800

    Update Extract-even-and-odd-numbers-from-a-text-file.py

---
+++ Extract-even-and-odd-numbers-from-a-text-file.py
@@ -0,0 +1,7 @@
+# Extract-even-and-odd-numbers-from-a-text-file_Marquez Francis Ivan B._BSCpE 1-5
+
+# Read numbers.txt
+# Sort even and odd numbers
+# Create even.txt and odd.txt
+# Append even numbers to even.txt
+# Append odd numbers to odd.txt
