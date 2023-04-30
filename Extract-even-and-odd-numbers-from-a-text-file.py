81dff8d6d1863713e0eb1fb642b67c2e0e0d30ef
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-04-30 15:51:53 +0800

    read numbers.txt working

--- Extract-even-and-odd-numbers-from-a-text-file.py
+++ Extract-even-and-odd-numbers-from-a-text-file.py
@@ -1,7 +1,12 @@
 # Extract-even-and-odd-numbers-from-a-text-file_Marquez Francis Ivan B._BSCpE 1-5
 
 # Read numbers.txt
-# Sort even and odd numbers
+with  open("numbers.txt", "r") as my_file:
+    for line in my_file:
+        print (line.strip())
+
+
+
 # Create even.txt and odd.txt
 # Append even numbers to even.txt
 # Append odd numbers to odd.txt
