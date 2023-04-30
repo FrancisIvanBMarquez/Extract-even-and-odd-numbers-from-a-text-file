9079cc20d7c02c20151e7053ead4602c695673ee
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-04-30 16:09:22 +0800

    Create even_numbers.txt and odd_numbers.txt

--- Extract-even-and-odd-numbers-from-a-text-file.py
+++ Extract-even-and-odd-numbers-from-a-text-file.py
@@ -1,16 +1,14 @@
 # Extract-even-and-odd-numbers-from-a-text-file_Marquez Francis Ivan B._BSCpE 1-5
 
 # Read numbers.txt
-with  open("numbers.txt", "r") as my_file:
-    for line in my_file:
-        print (line.strip())
+# Create files for even and odd numbers
+with  open("numbers.txt", "r") as my_file1, open ("even_numbers.txt", "a") as my_file2, open ("odd_numbers.txt", "a") as my_file3:
+    pass
+    #for line in my_file1:
+        # if else for reading lines
+        # if even
+        
 
-
-
-# if else for reading lines
-# if even
 # Append even numbers to even.txt
 # if odd
 # Append odd numbers to odd.txt
-
-
