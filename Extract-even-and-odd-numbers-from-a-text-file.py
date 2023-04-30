872d95a5bc810c48ebaf9e36e2b84fc2eaf8edde
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-04-30 16:25:38 +0800

    reading lines and appending to even_numbers.txt working

--- Extract-even-and-odd-numbers-from-a-text-file.py
+++ Extract-even-and-odd-numbers-from-a-text-file.py
@@ -3,10 +3,11 @@
 # Read numbers.txt
 # Create files for even and odd numbers
 with  open("numbers.txt", "r") as my_file1, open ("even_numbers.txt", "a") as my_file2, open ("odd_numbers.txt", "a") as my_file3:
-    pass
-    #for line in my_file1:
+    for line in my_file1:
         # if else for reading lines
         # if even
+        if (int(line) % 2) == 0:
+            my_file2.write(line)
         
 
 # Append even numbers to even.txt
